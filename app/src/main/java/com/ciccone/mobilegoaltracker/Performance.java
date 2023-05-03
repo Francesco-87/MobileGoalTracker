e6353b92e47333908e30ede83b716afec7c6834c
Francesco-87 <francesco.ciccone@iubh.de>
2023-05-03 19:39:59 +0200

    All layouts are done, but not finished; The first three intents to the different activities are done.

--- app/src/main/java/com/ciccone/mobilegoaltracker/Performance.java
+++ app/src/main/java/com/ciccone/mobilegoaltracker/Performance.java
@@ -2,13 +2,26 @@ package com.ciccone.mobilegoaltracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class Performance extends AppCompatActivity {
 
+    //setting all the variables
+    private ImageButton returnToMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_performance);
+
+        //assigning the layout to variables
+        returnToMain = findViewById(R.id.imgBtnReturnFromPerformance);
+
+        //implementing the return to mainActivity function
+        returnToMain.setOnClickListener(View ->{
+            Intent returnToMain = new Intent(Performance.this, MainActivity.class);
+            startActivity(returnToMain);
+        });
     }
 }
--- app/src/main/java/com/ciccone/mobilegoaltracker/Planning.java
+++ app/src/main/java/com/ciccone/mobilegoaltracker/Planning.java
@@ -2,13 +2,26 @@ package com.ciccone.mobilegoaltracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class Planning extends AppCompatActivity {
 
+    //setting all the variables
+    private ImageButton returnToMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_planning);
+
+        //assigning the layout to variables
+        returnToMain = findViewById(R.id.imgBtnReturnFromPlanning);
+
+        //implementing the return to mainActivity function
+        returnToMain.setOnClickListener(View ->{
+            Intent returnToMain = new Intent(Planning.this, MainActivity.class);
+            startActivity(returnToMain);
+        });
     }
 }
